3dae33b9cfa786fbcf5ab8f2e587c6f794e8400b
Alex96M <87329406+Alex96M@users.noreply.github.com>
2026-04-14 08:53:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -41,4 +41,6 @@ app.get("/signals", (req, res) => {
 });
 
 // === SERVIDOR ===
-app.listen(3000, () => console.log("Servidor corriendo"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log("Servidor corriendo en puerto " + PORT));
