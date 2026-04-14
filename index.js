9df85d8d406092a24cf90349550c0615dce113f9
Alex96M <87329406+Alex96M@users.noreply.github.com>
2026-04-14 09:01:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+// test ruta simple
 app.get("/", (req, res) => {
     res.send("Servidor funcionando 🚀");
+});
+
+// webhook simple
+app.post("/webhook", (req, res) => {
+    console.log("Webhook:", req.body);
+    res.send("OK");
 });
 
 const PORT = process.env.PORT || 3000;
